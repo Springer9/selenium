6757030c7c230b72efeae5875d5e1739ee72a4c5
Matthew Martinez <117482839+Tangerine-C@users.noreply.github.com>
2025-04-11 11:22:39 -0400

    Update CanvasAutomationTests.java

--- CanvasAutomationTests.java
+++ CanvasAutomationTests.java
@@ -50,8 +50,8 @@ public class CanvasAutomationTests {
         WebElement passwordField = driver.findElement(By.id("password"));
         WebElement loginButton = driver.findElement(By.id("loginButton"));
 
-        signInField.sendKeys("tlcramer7509@eagle.fgcu.edu");
-        passwordField.sendKeys("Thewhitedog");
+        signInField.sendKeys("EXAMPLE@eagle.fgcu.edu");
+        passwordField.sendKeys("EXAMPLE");
         loginButton.click();
 
         // Prompt for user action
